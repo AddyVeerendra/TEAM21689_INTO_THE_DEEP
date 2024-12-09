8e1114bd4425e32e38cc235766adc6bc464503b1
jdbott <jdbottino1208@gmail.com>
2024-12-08 19:53:45 -0500

    poopy fart intake works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiscTests/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MiscTests/IntakeClaw.java
@@ -7,16 +7,25 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Intake Claw Test", group = "TeleOp")
 public class IntakeClaw extends LinearOpMode {
 
-    // Declare the continuous rotation servo
+    // Declare the servos
     private Servo intakePivot;
     private Servo intakeRotate;
     private Servo intakeClaw;
     private Servo intakeSlidesLeft;
 
+    // Toggle states
+    private boolean clawOpen = false;
+    private boolean clawRotated = false;
+    private boolean sequenceToggle = false;
+
+    // Track button states
+    private boolean rightBumperPressed = false;
+    private boolean leftBumperPressed = false;
+    private boolean dpadRightPressed = false;
 
     @Override
     public void runOpMode() {
-        // Initialize the servo
+        // Initialize the servos
         intakePivot = hardwareMap.get(Servo.class, "intakePivot");
         intakePivot.setDirection(Servo.Direction.REVERSE);
         intakeRotate = hardwareMap.get(Servo.class, "intakeRotate");
@@ -27,28 +36,42 @@ public class IntakeClaw extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.dpad_left) {
-                intakeRotate.setPosition(0);
-            } else if (gamepad1.dpad_right) {
-                intakeRotate.setPosition(0.3);
+            // Right bumper toggles claw open/closed
+            if (gamepad1.right_bumper && !rightBumperPressed) {
+                clawOpen = !clawOpen;
+                intakeClaw.setPosition(clawOpen ? 1.0 : 0.75);
+                rightBumperPressed = true;
+            } else if (!gamepad1.right_bumper) {
+                rightBumperPressed = false;
             }
 
-            if (gamepad1.left_bumper) {
-                intakeClaw.setPosition(1);
-            } else if (gamepad1.right_bumper) {
-                intakeClaw.setPosition(0.75);
+            // Left bumper toggles claw rotation
+            if (gamepad1.left_bumper && !leftBumperPressed) {
+                clawRotated = !clawRotated;
+                intakeRotate.setPosition(clawRotated ? 0.3 : 0.0);
+                leftBumperPressed = true;
+            } else if (!gamepad1.left_bumper) {
+                leftBumperPressed = false;
             }
 
-            if (gamepad1.dpad_up) {
-                intakePivot.setPosition(0);
-            } else if (gamepad1.dpad_down){
-                intakePivot.setPosition(0.75);
-            }
-
-            if (gamepad1.left_trigger > 0.5) {
-                intakeSlidesLeft.setPosition(0);
-            } else if (gamepad1.right_trigger > 0.5) {
-                intakeSlidesLeft.setPosition(0.27);
+            // D-pad right toggles between sequences
+            if (gamepad1.dpad_right && !dpadRightPressed) {
+                sequenceToggle = !sequenceToggle;
+                if (sequenceToggle) {
+                    // Sequence 1
+                    intakeClaw.setPosition(0.0);
+                    sleep(300);
+                    intakeSlidesLeft.setPosition(0.3);
+                    intakePivot.setPosition(0.75);
+                } else {
+                    // Sequence 2
+                    intakeSlidesLeft.setPosition(0.7);
+                    intakePivot.setPosition(0.0);
+                    intakeClaw.setPosition(0.3);
+                }
+                dpadRightPressed = true;
+            } else if (!gamepad1.dpad_right) {
+                dpadRightPressed = false;
             }
         }
     }
